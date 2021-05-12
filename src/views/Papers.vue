170495301530bfddb98e5df17453b6a3b5082f95
Bruno CHIANCA <bruno.chianca@enac.fr>
2021-05-12 12:11:03 +0200

    Update

--- src/views/Papers.vue
+++ src/views/Papers.vue
@@ -27,24 +27,12 @@
             <v-list-item-text class="mt-4">
               <div class="overline mb-4">Abstract</div>
                 <div class="caption mb-4">
-                  In Wireless Sensor Networks (WSN), typically composed of nodes with resource constraints,
-                  leveraging efficient processes is crucial to enhance the network lifetime and, consequently,
-                  the sustainability in ultra-dense and heterogeneous environments, such as smart cities.
-                  <br>
-                  Particularly, balancing the energy required to transport data efficiently across such dynamic
-                  environments poses significant challenges to routing protocol design and operation, being the
-                  trade-off of reducing data redundancy while achieving an acceptable delivery rate a fundamental
-                  research topic.
-                  <br>
-                  In this way, this work proposes a new energy-aware epidemic protocol that uses
-                  the current state of the network energy to create a dynamic distribution topology by self-adjusting
-                  each node forwarding behavior as eager or lazy according to the local residual battery.
-                  Simulated evaluations demonstrate its efficiency in energy consumption, delivery rate, and reduced
-                  computational burden when compared with classical gossip protocols as well as with a directional protocol.
+              {{ paper.abstract }}
                 </div>
             </v-list-item-text>
           </v-list-item-content>
         </v-list-item>
+
         <v-btn
             color="blue-grey"
             class="ma-2 white--text"
